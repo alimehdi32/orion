26ff99915babbbb300c073bef6fe11cc378fb31a
Ali Mehdi <alimehdi432faiz@gmail.com>
2025-06-16 20:13:39 +0530

    Project completed

--- src/components/ChatInterface.tsx
+++ src/components/ChatInterface.tsx
@@ -30,6 +30,7 @@ export default function ChatInterface() {
       role: 'assistant',
       content: newMessages[0].content[0].text,
     }])
+    setIsSubmitting(false);
 
     const res = await fetch('/api/fileName', {
       method: 'POST',
@@ -39,7 +40,6 @@ export default function ChatInterface() {
     });
     const { messages: fileName } = await res.json();
     setFiles(prev => [...prev, fileName[0].content[0].text]);
-    setIsSubmitting(false);
   }
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -64,14 +64,12 @@ export default function ChatInterface() {
   };
 
 
-
-
   return (
     <div className="flex flex-col md:flex-row justify-between w-full">
       <div className="w-full md:w-[300px]">
         <FileTree filename={files} onSelectFile={handleSelectFile} selectedFile={selectedFile} />
       </div>
-
+      
       <div className="flex flex-col flex-1 overflow-auto relative w-full">
         {/* Messages */}
         <div className="flex-1 overflow-y-auto space-y-4 mb-28 px-4">
@@ -98,17 +96,18 @@ export default function ChatInterface() {
           onSubmit={handleSubmit}
           className="flex gap-2 fixed bottom-4 left-4 right-4 md:left-[320px] md:right-6 bg-[#18181b] p-4 rounded-lg shadow-lg z-10 w-auto"
         >
-          <input
+          {isSubmitting ? <div className='text-2xl font-bold text-indigo-700'>Generating ...</div>
+          :<input
             type="text"
             value={input}
             onChange={(e) => setInput(e.target.value)}
             placeholder="Ask about Solana smart contracts..."
             className="flex-1 rounded-lg border border-gray-300 px-4 py-2 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 text-sm"
-          />
+          />}
           <button
             disabled={isSubmitting}
             type="submit"
-            className="rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            className="rounded-lg bg-blue-600 px-4 py-2 border-r border-t border-b border-gray-300 fixed left-[1445px] bottom-[32px] text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
           >
             {isSubmitting
               ? <Loader2 className="w-6 h-6 animate-spin text-blue-500" />
